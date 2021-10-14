e4e0f60d0695fdfcf29c7fcfc3890754b4bbbe73
unknown <ethan.s.tran@student.uts.edu.au>
2021-10-14 17:53:22 +1100

    Start Screen (50%) complete

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -13,6 +14,17 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    
+    // Loads the Level 1 scene
+    public void LoadLevel1() {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(1);
+        // SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         
     }
 }
