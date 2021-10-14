63ebbeb82ab946edb140af71dff9d48e96db1915
unknown <ethan.s.tran@student.uts.edu.au>
2021-10-14 14:21:40 +1100

    Catch-up from Assessment 3 complete (30%)

--- Assets/Scripts/PacMovement.cs
+++ Assets/Scripts/PacMovement.cs
@@ -18,7 +18,7 @@ public class PacMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        addTween();
+        // addTween();
         
 
         if (activeTween != null) {
@@ -39,50 +39,50 @@ public class PacMovement : MonoBehaviour
 
     }//end Update()
 
-    public void addTween() {
-        if (activeTween == null) {
+    // public void addTween() {
+    //     if (activeTween == null) {
 
-            //if pacman is in top left corner, move right
-            if ( (Obj.transform.position.x == -12.5f) && (Obj.transform.position.y == 13.5f) ) {
-                activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-7.5f, 13.5f, 0.0f), Time.time, 1.5f);
-                anim.ResetTrigger("walk-left");
-                anim.ResetTrigger("walk-up");
-                anim.ResetTrigger("walk-down");
-                anim.SetTrigger("walk-right");            
-            }
+    //         //if pacman is in top left corner, move right
+    //         if ( (Obj.transform.position.x == -12.5f) && (Obj.transform.position.y == 13.5f) ) {
+    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-7.5f, 13.5f, 0.0f), Time.time, 1.5f);
+    //             anim.ResetTrigger("walk-left");
+    //             anim.ResetTrigger("walk-up");
+    //             anim.ResetTrigger("walk-down");
+    //             anim.SetTrigger("walk-right");            
+    //         }
 
-            //if pacman is in top right corner, move down
-            if ( (Obj.transform.position.x == -7.5f) && (Obj.transform.position.y == 13.5f) ) {
-                activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-7.5f, 9.5f, 0.0f), Time.time, 1.5f);
-                anim.ResetTrigger("walk-left");
-                anim.ResetTrigger("walk-up");
-                anim.ResetTrigger("walk-right");
-                anim.SetTrigger("walk-down");   
-            }
+    //         //if pacman is in top right corner, move down
+    //         if ( (Obj.transform.position.x == -7.5f) && (Obj.transform.position.y == 13.5f) ) {
+    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-7.5f, 9.5f, 0.0f), Time.time, 1.5f);
+    //             anim.ResetTrigger("walk-left");
+    //             anim.ResetTrigger("walk-up");
+    //             anim.ResetTrigger("walk-right");
+    //             anim.SetTrigger("walk-down");   
+    //         }
 
-            //if pacman is in bottom right corner, move left
-            if ( (Obj.transform.position.x == -7.5f) && (Obj.transform.position.y == 9.5f) ) {
-                activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-12.5f, 9.5f, 0.0f), Time.time, 1.5f);
-                anim.ResetTrigger("walk-right");
-                anim.ResetTrigger("walk-up");
-                anim.ResetTrigger("walk-down");
-                anim.SetTrigger("walk-left");
-            }
+    //         //if pacman is in bottom right corner, move left
+    //         if ( (Obj.transform.position.x == -7.5f) && (Obj.transform.position.y == 9.5f) ) {
+    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-12.5f, 9.5f, 0.0f), Time.time, 1.5f);
+    //             anim.ResetTrigger("walk-right");
+    //             anim.ResetTrigger("walk-up");
+    //             anim.ResetTrigger("walk-down");
+    //             anim.SetTrigger("walk-left");
+    //         }
 
-            //if pacman is in bottom left corner, move up
-            if ( (Obj.transform.position.x == -12.5f) && (Obj.transform.position.y == 9.5f) ) {
-                activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-12.5f, 13.5f, 0.0f), Time.time, 1.5f);
-                anim.ResetTrigger("walk-left");
-                anim.ResetTrigger("walk-right");
-                anim.ResetTrigger("walk-down");
-                anim.SetTrigger("walk-up");
-            }
+    //         //if pacman is in bottom left corner, move up
+    //         if ( (Obj.transform.position.x == -12.5f) && (Obj.transform.position.y == 9.5f) ) {
+    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-12.5f, 13.5f, 0.0f), Time.time, 1.5f);
+    //             anim.ResetTrigger("walk-left");
+    //             anim.ResetTrigger("walk-right");
+    //             anim.ResetTrigger("walk-down");
+    //             anim.SetTrigger("walk-up");
+    //         }
 
             //for pacman death sprite, set trigger to play death animation
-            if ( (Obj.transform.position.x == -5.5f) && (Obj.transform.position.y == 0.5f) ) {
-                anim.SetTrigger("death-state");
-            }
-        }
-    }//end addTween()
+            // if ( (Obj.transform.position.x == -5.5f) && (Obj.transform.position.y == 0.5f) ) {
+            //     anim.SetTrigger("death-state");
+            // }
+    //     }
+    // }//end addTween()
 
 }//end class
