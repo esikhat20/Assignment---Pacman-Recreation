20f1415eaecbc50dbb91075751646c210c86b1a1
unknown <ethan.s.tran@student.uts.edu.au>
2021-10-20 17:02:07 +1100

    Pacman movement completed. Working on the particle effect and cherry script next

--- Assets/Scripts/ImageGlow.cs
+++ Assets/Scripts/ImageGlow.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextGlow : MonoBehaviour
+public class ImageGlow : MonoBehaviour
 {
     // subtitle rgb values
     private float r1 = (102.0f/255.0f);
     private float g1 = (51.0f/255.0f);
     private float b1 = (153.0f/255.0f);
 
-    // background rgb values
-    private float r2 = (20.0f/255.0f);
-    private float g2 = (20.0f/255.0f);
-    private float b2 = (20.0f/255.0f);
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +20,7 @@ public class TextGlow : MonoBehaviour
     void Update()
     {
         // Transition between colours specified to give a "breathing" effect for the subtitle
-        GetComponent<Text>().color = Color.Lerp(new Color(r1, g1, b1), new Color(r2, g2, b2), Mathf.PingPong(Time.time, 1.25f));
+        GetComponent<Image>().color = Color.Lerp(new Color(r1, g1, b1), Color.clear, Mathf.PingPong(Time.time, 1.25f));
         
     }
 }
--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -7,25 +7,42 @@ public class PacStudentController : MonoBehaviour
     private GameObject Obj;
     private Tween activeTween;
     private Animator anim;
+    private AudioSource pacAudioSource;
+    private string lastInput;
+    private string currentInput;
+    private float distance;
     
 
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        pacAudioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // addTween();
-        
+        checkLastInput();
+
+        // if the raycast does not hit an object (i.e. wall), then continue movement
+        if (!RaycastCheck()) {
+            currentInput = lastInput;
+            anim.speed = 1;
+            pacAudioSource.UnPause();
+            addTween();
+        }
+        // else stop animation and walking sound
+        else {
+            anim.speed = 0;
+            pacAudioSource.Pause();
+        }
 
         if (activeTween != null) {
 
             //calculating lerp variables using linear interpolation since pacman has linear movement
-            float timeFraction = (Time.time-activeTween.StartTime) / activeTween.Duration;
-            float distance = Vector3.Distance(activeTween.Target.position, activeTween.EndPos);
+            float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+            distance = Vector3.Distance(activeTween.Target.position, activeTween.EndPos);
 
             if (distance > 0.1f) {
                 activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
@@ -35,54 +52,116 @@ public class PacStudentController : MonoBehaviour
                 activeTween.Target.position = activeTween.EndPos;
                 activeTween = null;
             }
-        }
+        }        
 
     }//end Update()
 
-    // public void addTween() {
-    //     if (activeTween == null) {
-
-    //         //if pacman is in top left corner, move right
-    //         if ( (Obj.transform.position.x == -12.5f) && (Obj.transform.position.y == 13.5f) ) {
-    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-7.5f, 13.5f, 0.0f), Time.time, 1.5f);
-    //             anim.ResetTrigger("walk-left");
-    //             anim.ResetTrigger("walk-up");
-    //             anim.ResetTrigger("walk-down");
-    //             anim.SetTrigger("walk-right");            
-    //         }
-
-    //         //if pacman is in top right corner, move down
-    //         if ( (Obj.transform.position.x == -7.5f) && (Obj.transform.position.y == 13.5f) ) {
-    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-7.5f, 9.5f, 0.0f), Time.time, 1.5f);
-    //             anim.ResetTrigger("walk-left");
-    //             anim.ResetTrigger("walk-up");
-    //             anim.ResetTrigger("walk-right");
-    //             anim.SetTrigger("walk-down");   
-    //         }
-
-    //         //if pacman is in bottom right corner, move left
-    //         if ( (Obj.transform.position.x == -7.5f) && (Obj.transform.position.y == 9.5f) ) {
-    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-12.5f, 9.5f, 0.0f), Time.time, 1.5f);
-    //             anim.ResetTrigger("walk-right");
-    //             anim.ResetTrigger("walk-up");
-    //             anim.ResetTrigger("walk-down");
-    //             anim.SetTrigger("walk-left");
-    //         }
-
-    //         //if pacman is in bottom left corner, move up
-    //         if ( (Obj.transform.position.x == -12.5f) && (Obj.transform.position.y == 9.5f) ) {
-    //             activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (-12.5f, 13.5f, 0.0f), Time.time, 1.5f);
-    //             anim.ResetTrigger("walk-left");
-    //             anim.ResetTrigger("walk-right");
-    //             anim.ResetTrigger("walk-down");
-    //             anim.SetTrigger("walk-up");
-    //         }
+    public void addTween() {
+        if (activeTween == null) {
+            // move up
+            if (currentInput == "W") {
+                if (distance < 0.2f) { // delay for lastInput 
+                activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (transform.position.x, transform.position.y + 1.0f, 0.0f), Time.time, 0.35f);
+                anim.ResetTrigger("walk-left");
+                anim.ResetTrigger("walk-down");
+                anim.ResetTrigger("walk-right");
+                anim.SetTrigger("walk-up");
+                }
+            }
+            // move left
+            else if (currentInput == "A") {
+                if (distance < 0.2f) {
+                    activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (transform.position.x - 1.0f, transform.position.y, 0.0f), Time.time, 0.35f);
+                    anim.ResetTrigger("walk-down");
+                    anim.ResetTrigger("walk-right");
+                    anim.ResetTrigger("walk-up");
+                    anim.SetTrigger("walk-left");
+                }
+            }
+            //move down
+            else if (currentInput == "S") {
+                if (distance < 0.2f) {
+                    activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (transform.position.x, transform.position.y - 1.0f, 0.0f), Time.time, 0.35f);
+                    anim.ResetTrigger("walk-right");
+                    anim.ResetTrigger("walk-up");
+                    anim.ResetTrigger("walk-left");
+                    anim.SetTrigger("walk-down");
+                }
+            }
+            //move right
+            else if (currentInput == "D") {
+                if (distance < 0.2f) {
+                    activeTween = new Tween (Obj.transform, Obj.transform.position, new Vector3 (transform.position.x + 1.0f, transform.position.y, 0.0f), Time.time, 0.35f);
+                    anim.ResetTrigger("walk-up");
+                    anim.ResetTrigger("walk-left");
+                    anim.ResetTrigger("walk-down");
+                    anim.SetTrigger("walk-right");
+                }
+            }
 
             //for pacman death sprite, set trigger to play death animation
             // if ( (Obj.transform.position.x == -5.5f) && (Obj.transform.position.y == 0.5f) ) {
             //     anim.SetTrigger("death-state");
             // }
-    //     }
-    // }//end addTween()
+        }
+    }//end addTween()
+
+    // Checks the value of lastInput
+    public void checkLastInput() {
+        
+        if (Input.GetKeyDown(KeyCode.W)) {
+            lastInput = "W";
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            lastInput = "A";
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) {
+            lastInput = "S";
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            lastInput = "D";
+        }
+    }
+
+    // Checks if pacman will hit a wall using raycasts
+    public bool RaycastCheck() {
+        
+        // Casts a ray straight up, left, down or right
+        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, 1.0f);
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, 1.0f);
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, 1.0f);
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down, 1.0f);
+
+        bool wasHit = false;
+        
+        switch (lastInput) {
+
+            case "W":
+                if (hitUp.collider != null) {
+                    return true;
+                }
+                break;
+            
+            case "A":
+                if (hitLeft.collider != null) {
+                    return true;
+                }
+                break;
+            
+            case "S":
+                
+                if (hitDown.collider != null) {
+                    return true;
+                }
+                break;
+
+            case "D":
+                if (hitRight.collider != null) {
+                    return true;
+                }
+                break;
+        }//end switch
+        return wasHit;
+    }//end RaycastCheck()
 
 }//end class
